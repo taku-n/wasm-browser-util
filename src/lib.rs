baa6f5cb5a6211ceebfe7b92d37a486f92c8220d
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2021-11-23 16:18:13 +0900

    run_local()

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,47 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-
-use std::cell::Cell;
-use std::rc::Rc;
-use std::sync::mpsc;
-
+use futures::channel::oneshot;
 use futures::prelude::*;
 
-pub async fn spawn() -> i32 {
-    let (mut tx, mut rx) = futures::channel::mpsc::unbounded();
-    let output = Rc::new(Cell::new(0));
-    let output_move = output.clone();
+// Usage:
+//  let future = run_local::<return-type-of-async-block, _>(async [move] {});
+// Example:
+//  let future = run_local::<i32, _>(async {2});
+//  future.await.unwrap();  // 2 of i32
+// I'm not sure but I guess the static lifetime is needed
+// for the time the spawned thread remains longer than the caller.
+pub fn run_local<T, Fut>(future: Fut) -> impl Future<Output = Result<T, oneshot::Canceled>>
+        where
+                T: 'static,
+                Fut: Future<Output = T> + 'static,
+{
+    let (tx, rx) = oneshot::channel();
 
-    let async_block = async move {
-        output_move.set(1);
-        console::log_1(&"inside".into());
-        console::log_1(&output_move.get().into());
-        tx.send(2).await;
-        ()
+    let future_with_tx = async move {
+        let result = future.await;
+
+        // unwrap() method cannot be called on `Result<(), T>` due to unsatisfied trait bounds
+        // T: Debug
+        tx.send(result).unwrap_or(());  // Better way?
     };
-    wasm_bindgen_futures::spawn_local(async_block);
+    wasm_bindgen_futures::spawn_local(future_with_tx);
 
-    console::log_1(&"outside".into());
-    console::log_1(&output.get().into());
-    output.get();
-    rx.next().await.unwrap()
+    rx
 }
 
-pub async fn test_spawn() {
+pub async fn test_run_local() {
     console::log_1(&"test_spawn() begins.".into());
-    let output = spawn();
-    // Some other tasks.
-    console::log_1(&output.await.into());
+    let future = run_local::<i32, _>(async {
+        2
+    });
+    let two = future.await.unwrap();
+    console::log_1(&two.into());  //=> 2
     console::log_1(&"test_spawn() ends.".into());
 }
 
 #[wasm_bindgen(start)]
 pub async fn main() {
     console::log_1(&"main() begins.".into());
-    test_spawn().await;
+    test_run_local().await;
     console::log_1(&"main() ends.".into());
 }
